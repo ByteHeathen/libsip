1a14822d2f1e18010a513360483d5b2fe72bd1fc
Patrick Greene <pmg41494@protonmail.com>
2020-05-09 09:31:45 -0500

    move ContentType to new parsers

--- src/headers/content.rs
+++ src/headers/content.rs
@@ -46,22 +46,31 @@ impl fmt::Display for ContentType {
     }
 }
 
-named!(pub parse_content_type<ContentType>, alt!(
-    map!(tag_no_case!("application/sdp"), |_| ContentType::Sdp) |
-    map!(tag_no_case!("text/plain"), |_| ContentType::PlainText) |
-    map!(tag_no_case!("text/html"), |_| ContentType::Html) |
-    map!(tag_no_case!("applications/xml"), |_| ContentType::Xml) |
-    map!(tag_no_case!("text/vcard"), |_| ContentType::VCard) |
-    map!(tag_no_case!("text/calendar"), |_| ContentType::Calendar) |
-    map!(tag_no_case!("text/markdown"), |_| ContentType::MarkDown) |
-    map!(tag_no_case!("application/msword"), |_| ContentType::MsWord) |
-    map!(tag_no_case!("application/pdf"), |_| ContentType::Pdf) |
-    map!(tag_no_case!("image/png"), |_| ContentType::Png) |
-    map!(tag_no_case!("text/csv"), |_| ContentType::Csv) |
-    map!(tag_no_case!("application/zip"), |_| ContentType::Zip) |
-    map!(tag_no_case!("application/sql"), |_| ContentType::Sql) |
-    map!(tag_no_case!("application/json"), |_| ContentType::Json) |
-    map!(tag_no_case!("application/gzip"), |_| ContentType::GZip) |
-    map!(tag_no_case!("application/javascript"), |_| ContentType::Javascript) |
-    map!(tag_no_case!("application/css"), |_| ContentType::Css)
-));
+use nom::{
+    IResult,
+    branch::alt,
+    combinator::map,
+    bytes::complete::tag_no_case
+};
+
+pub fn parse_content_type(input: &[u8]) -> IResult<&[u8], ContentType> {
+    Ok(alt((
+      map(tag_no_case("application/sdp"), |_| ContentType::Sdp),
+      map(tag_no_case("text/plain"), |_| ContentType::PlainText),
+      map(tag_no_case("text/html"), |_| ContentType::Html),
+      map(tag_no_case("applications/xml"), |_| ContentType::Xml),
+      map(tag_no_case("text/vcard"), |_| ContentType::VCard),
+      map(tag_no_case("text/calendar"), |_| ContentType::Calendar),
+      map(tag_no_case("text/markdown"), |_| ContentType::MarkDown),
+      map(tag_no_case("application/msword"), |_| ContentType::MsWord),
+      map(tag_no_case("application/pdf"), |_| ContentType::Pdf),
+      map(tag_no_case("image/png"), |_| ContentType::Png),
+      map(tag_no_case("text/csv"), |_| ContentType::Csv),
+      map(tag_no_case("application/zip"), |_| ContentType::Zip),
+      map(tag_no_case("application/sql"), |_| ContentType::Sql),
+      map(tag_no_case("application/json"), |_| ContentType::Json),
+      map(tag_no_case("application/gzip"), |_| ContentType::GZip),
+      map(tag_no_case("application/javascript"), |_| ContentType::Javascript),
+      map(tag_no_case("application/css"), |_| ContentType::Css)
+    ))(input)?)
+}
